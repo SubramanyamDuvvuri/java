ba656f0ea022069cfd18d5133a122bee68855fc0
subramanyamduvvuri <duvvurisubramanyam@gmail.com>
2015-08-09 19:59:35 +0200

    taking an input and printing it

---
+++ Third_program.java
@@ -0,0 +1,13 @@
+/* taking an input from the keyboard */
+
+import java.util.Scanner;
+
+class Third_program
+{
+	public static void main (String[] arg )
+	{ 
+		Scanner x = new Scanner (System.in );
+		System.out.println ( x.nextLine() ) ;
+	} 
+}
+	
