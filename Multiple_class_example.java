04822b5c98dd3e3669dea525bfc1354759dbef2c
subramanyamduvvuri <duvvurisubramanyam@gmail.com>
2015-08-10 00:13:55 +0200

    example for using multiple classes and calling from differnt files

---
+++ Multiple_class_example.java
@@ -0,0 +1,18 @@
+/*multiple class usage */
+
+class Multiple_class_example
+
+{
+	public static void main ( String [] args )
+	{	
+		System.out.println ( " in class main\n " );
+		
+		Multiple_class_example_call object = new Multiple_class_example_call();
+		
+		object.printMessage ();
+
+		
+	}
+
+}
+
---
+++ Multiple_class_example_call.java
@@ -0,0 +1,9 @@
+/* the called class*/
+
+public class Multiple_class_example_call
+{
+	public static void  printMessage ()
+	{
+		System.out.println ( " in class two" ) ;
+	}
+}
