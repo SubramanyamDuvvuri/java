42552646afe380a9c0f0ab110bd92bc91c14bf79
subramanyamduvvuri <duvvurisubramanyam@gmail.com>
2015-08-29 12:52:50 +0200

    initial commit some basic tutorials

---
+++ multiple_classes_and_methods.java
@@ -0,0 +1,12 @@
+/* Calling class , will call multiple classes*/
+import java.util.Scanner;
+
+
+class multiple_classes_and_methods
+{
+	public static void main (String args [] )
+	{
+		Scanner input = new Scanner (System.in);
+		String name = input.nextLine();
+	
+
