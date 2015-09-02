648519297331eb9c7fb176d90c4973a3c211e697
subramanyamduvvuri <duvvurisubramanyam@gmail.com>
2015-09-02 10:06:44 +0200

    simple method

---
+++ methodwork.java
@@ -0,0 +1,16 @@
+class methodwork 
+{
+	public static void main (String [] arg)
+	{
+		System.out.println (" message 1");
+		displayMessage ();
+		System.out.println ("message 3");
+		
+	}
+	
+	public static void displayMessage()
+	{
+		System.out.println("messgage 2");
+	
+	}
+}
