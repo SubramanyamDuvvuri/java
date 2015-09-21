2fa9597bf0f3ca97d4ac40b89591e3287c059223
subramanyamduvvuri <duvvurisubramanyam@gmail.com>
2015-09-22 01:21:14 +0200

    withoout using the constructor in the main program

---
+++ Rectangle.java
@@ -0,0 +1,44 @@
+ class Rectangle
+{
+	private double length;
+	private double width;
+
+	
+	// the setlength () method is used to set the length of the rectangle
+	public   void setlength ( double len)
+	{
+	 	length = len ;
+	}
+	
+	//setwidth method is used to set the width of the rectangle
+	public void  setwidth( double wid)
+	{
+		width=wid ;
+	}
+
+	
+	//for displaying the length
+	public double getlength()
+	{
+		return length;
+	}
+	
+	//for displaying the width
+	public double getwidth()
+	{
+		return width;
+
+	}
+
+	// Constructor , Should have the same name as of the class and should not have a return type not even void  
+
+	public Rectangle( double l , double width )
+	{
+		length = l;
+		width = w;
+	}
+
+	
+	 
+	
+}
---
+++ ShapeDemo.java
@@ -0,0 +1,15 @@
+/*  first  class creation example */
+class ShapeDemo
+{
+	public static void main ( String [] arg )
+	{
+		Rectangle box = new Rectangle();
+	
+		box.setlength(10.19);
+		box.setwidth( 9.33);
+		
+		System.out.println ( box.getlength ());
+	}
+	
+}
+	
