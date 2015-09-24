fbc8f2ea0bc1eb2a37ac67ab0918484e5d793d6f
subramanyamduvvuri <duvvurisubramanyam@gmail.com>
2015-09-24 20:53:11 +0200

    when calling a function from the main satic function all functions should be static

--- examplearray.java
+++ examplearray.java
@@ -1,17 +1,36 @@
 class examplearray 
 {
-	public static void main ( String [] arg )
+	public  void main ( String [] arg )
 	{
 		final int MAX = 6;
 		int number [] = new int  [MAX];	
 				
-		for ( int i = 0 ; i < number.length ; i++)
+		for ( int i = 0 ; i < 2 ; i++)
 		{
-			number[i] = i;
+			number[i] = i+7;
 		}
 	 
 		
-		  
-		System.out.println (number[3]);
+		for ( int i = 0 ; i < number.length ; i++)
+		{
+			System.out.println (number[i]);
+		}
+		show (number);
+
 	}
+
+
+	public  void show ( int n [] ) 
+	{
+
+			for ( int i = 0 ; i < n.length ; i++)
+		{
+			System.out.println (n[i]);
+		}
+		  
+	}
+		  
+		  
+		  
+
 }
