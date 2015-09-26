4f53312c2282f1072ce644a2c69fde382f624863
subramanyamduvvuri <duvvurisubramanyam@gmail.com>
2015-09-26 02:03:36 +0200

    array of objects

--- studentinfo.java
+++ studentinfo.java
@@ -4,22 +4,35 @@ class studentinfo
 {
 	public static void main ( String [] arg )
 	 {
-		String Subjects[] = new String [4];
+		String subjects[] = new String [4];
 
-		System.out.println ( " enter the subjects" );
 		Scanner input = new Scanner ( System.in);
-		for( int  i = 0 ; i < Subjects.length ; i++ )
+		
+		//Creating array of objects 
+		student student_info [] = new student [3];
+		
+		for (int j=0 ; j< student_info.length ; j++) 
 		{
-			Subjects[i]= input.nextLine();
-		}
-		System.out.println ( "Enter the School name " );
-		String School = input.nextLine();
-		student student1 = new student("Billu", Subjects ,School);
+				System.out.println (" Enter the name of the student ----> " +j);
+				String temp_name = input.nextLine();		
+				
+				System.out.println ( " enter the subjects" );
+	
+				for( int  i = 0 ; i < subjects.length ; i++ )
+				{
+					subjects[i]= input.nextLine();
+				}
+				System.out.println ( "enter the school name " );
+				String school = input.nextLine();
+				student_info[j] = new student(temp_name , subjects , school );
+		}			
+
+
 		System.out.println ( " Student name is ");
-		System.out.println (student1.name );
+		System.out.println (student_info[1].name );
 		System.out.println (" enter the percenttage to get the grade " );
 		int percentage = input.nextInt();
-		System.out.println("The grade obtaied by the student is " +   	student1.get_grade( percentage ));
+		System.out.println("The grade obtaied by the student is " +   	student_info[1].get_grade( percentage ));
 		
 	
 				 
